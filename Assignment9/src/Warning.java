8e2a33de376e890ce4d5af884b660b8616655ec2
Ejulu Wilson <emox1995@gmail.com>
2022-11-19 05:38:27 +0300

    complets qtn 3 of assgnment 9

--- Assignment9/src/Warning.java
+++ Assignment9/src/Warning.java
@@ -18,20 +18,48 @@ public class Warning {
         double gpa; // grade point (quality point) average
         String line, name, inputName = "students.dat";
         String outputName = "warning.dat";
+
         try {
-// Set up scanner to input file
-// Set up the output file stream
-// Print a header to the output file
+            // Set up scanner to input file
+            File myObj = new File("P:\\Java\\Assignment9\\"+inputName);
+            Scanner scan = new Scanner(myObj);
+
+            // Set up the output file stream
+            PrintWriter outFile = new PrintWriter("P:\\Java\\Assignment9\\"+outputName);
+
+            // Print a header to the output file
             outFile.println();
             outFile.println("Students on Academic Warning");
-            outFile.println ();
-// Process the input file, one token at a time
-            while () {
-// Get the credit hours and quality points and
-// determine if the student is on warning. If so,
-// write the student data to the output file.
+            outFile.println();
+
+            // Process the input file, one token at a time
+            while (scan.hasNextLine()) {
+            // Get the credit hours and quality points and
+                name = scan.next();
+                creditHrs = Integer.parseInt(scan.next());
+                qualityPts = Double.parseDouble(scan.next());
+                gpa = qualityPts/creditHrs;
+
+                line = name+" "+creditHrs+" "+gpa;
+
+            // determine if the student is on warning. If so,
+            // write the student data to the output file.
+                if  ((gpa<1.5) && (creditHrs < 30)){
+                    // put on academic warning
+                    outFile.println(line);
+
+                } else if ((gpa<1.75) && (creditHrs < 60)){
+                    // put on academic warning
+                    outFile.println(line);
+
+                } else if (gpa<2.0) {
+                    // put on academic warning
+                    outFile.println(line);
+                }
             }
-// Close output file
+        // Close output file
+            outFile.close();
+
         } catch (FileNotFoundException exception) {
             System.out.println("The file " + inputName + " was not found.");
         } catch (IOException exception) {
