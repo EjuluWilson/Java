5d5e27c30997c7ca7d359cac9d0bc8b881cfe4aa
Ejulu Wilson <emox1995@gmail.com>
2022-11-19 06:17:48 +0300

    completed assignment 9

--- Assignment9/src/Factorials.java
+++ Assignment9/src/Factorials.java
@@ -8,14 +8,21 @@ public class Factorials
     public static void main(String[] args)
     {
         String keepGoing = "y";
-        Scanner scan = new Scanner(System.in);while (keepGoing.equals("y") || keepGoing.equals("Y"))
-    {
-        System.out.print("Enter an integer: "); int val
-            = scan.nextInt();
-        System.out.println("Factorial(" + val + ") = "
-                + MathUtils.factorial(val));
-        System.out.print("Another factorial? (y/n)
-                "); keepGoing = scan.next();
-    }
+        Scanner scan = new Scanner(System.in);
+        while (keepGoing.equalsIgnoreCase("Y"))
+        {
+            System.out.print("Enter an integer: ");
+            int val = scan.nextInt();
+
+            try {
+                System.out.println("Factorial(" + val + ") = "
+                        + MathUtils.factorial(val));
+
+            } catch (IllegalArgumentException ex) {
+                System.err.println(ex + "\n");
+            }
+            System.out.println("Another factorial? (y/n) ");
+            keepGoing = scan.next();
+        }
     }
 }
--- Assignment9/src/MathUtils.java
+++ Assignment9/src/MathUtils.java
@@ -8,9 +8,16 @@ public class MathUtils {
     //-------------------------------------------------------------
 // Returns the factorial of the argument given
 //-------------------------------------------------------------
-    public static int factorial(int n) {
+    public static int factorial(int n) throws IllegalArgumentException
+    {
+        if (n < 0) {
+            throw new IllegalArgumentException(n + " is a negative number");
+        } else if (n > 16) {
+            throw new IllegalArgumentException(n + " is far too large");
+        }
+
         int fac = 1;
-        for (int i = n; i > 0; i--)
+        for (int i=n; i>0; i--)
             fac *= i;
         return fac;
     }
